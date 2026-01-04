abef42fcb2d46181a5a48a08cacfa82e32319db0
Awwal2007 <awwalsaminu9@gmail.com>
2026-01-04 15:23:10 +0100

    sdfsd

--- src/contexts/BankingContext.jsx
+++ src/contexts/BankingContext.jsx
@@ -7,7 +7,7 @@ export const useBanking = () => useContext(BankingContext)
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
